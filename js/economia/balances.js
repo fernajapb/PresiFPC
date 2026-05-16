b7b90193f75f19ab21a551b251dc10b53044c4a6
fernajapb <fernaaguilarpau@gmail.com>
2026-05-15 23:30:40 -0500

    Add files via upload

---
+++ js/economia/balances.js
@@ -0,0 +1,74 @@
+
+
+/*Balances*/
+
+let balanceEconomico = {
+  sponsor: 0,
+  sueldos: 0,
+  derechos: 0,
+  camisetas: 0,
+  taquilla: 0 ,
+  alquiler: 0,
+  ventas: 0,
+  compras: 0
+};
+
+function actualizarBalanceUI() {
+  actualizarLinea("balanceSponsor", balanceEconomico.sponsor);
+  actualizarLinea("balanceSueldos", balanceEconomico.sueldos);
+  actualizarLinea("balanceDerechos", balanceEconomico.derechos);
+  actualizarLinea("balanceCamisetas", balanceEconomico.camisetas);
+  actualizarLinea("balanceTaquilla", balanceEconomico.taquilla); 
+  actualizarLinea("balanceAlquiler", balanceEconomico.alquiler); 
+  actualizarLinea("balanceVentas", balanceEconomico.ventas); 
+  actualizarLinea("balanceCompras", balanceEconomico.compras); 
+
+  // ❗ TOTAL SIN SUELDOS
+   const total =
+  balanceEconomico.sponsor +
+  balanceEconomico.derechos +
+  balanceEconomico.camisetas +
+  balanceEconomico.taquilla +
+  balanceEconomico.alquiler +
+  balanceEconomico.sueldos +
+  balanceEconomico.ventas +
+  balanceEconomico.compras
+
+  actualizarLinea("balanceTotal", total);
+}
+
+/*
+function actualizarLinea(id, monto) {
+  const el = document.getElementById(id);
+  if (!el) return;
+
+  el.className = monto > 0 ? "positivo" : monto < 0 ? "negativo" : "neutral";
+  el.textContent = `${monto >= 0 ? "+" : "-"}$${Math.abs(monto / 1e6).toFixed(1)}M`;
+}
+*/
+
+function actualizarLinea(id, monto) {
+  const el = document.getElementById(id);
+  if (!el) return;
+
+  el.className = monto > 0 ? "positivo" : monto < 0 ? "negativo" : "neutral";
+
+  const valorFormateado = formatearPrecio(Math.abs(monto));
+
+  el.textContent = `${monto >= 0 ? "+" : "-"}${valorFormateado}`;
+}
+
+function resetearBalanceEconomico() {
+  balanceEconomico = {
+    sponsor: 0,
+    sueldos: 0,
+    derechos: 0,
+    camisetas: 0,
+    taquilla: 0,
+    alquiler: 0,
+    ventas: 0,
+    compras: 0
+  };
+
+  actualizarBalanceUI();
+}
---
+++ js/economia/finanzas.js
@@ -0,0 +1,164 @@
+
+let derechosReclamados = false;
+let decisionSueldosTomada = false;
+let alquilerPagado = false;
+
+function abrirModalEconomico() {
+
+  const montoDerechosTV = derechosTV?.monto || 1500000;
+  const sueldoAnual = calcularSueldoPorFuerza(
+    obtenerFuerzaTotal(equipoUsuario)
+  );
+
+  document.getElementById("infoDerechos").innerText =
+    `Derechos de TV: $${montoDerechosTV.toLocaleString()}`;
+
+  document.getElementById("infoSueldos").innerText =
+    `Sueldos de plantilla: $${sueldoAnual.toLocaleString()}`;
+
+  document.getElementById("modalEconomico").style.display = "flex";
+
+  derechosReclamados = false;
+  decisionSueldosTomada = false;
+  alquilerPagado = false;
+  document.getElementById("btnPagarAlquiler").disabled = false;
+}
+
+//Reclamar derechos 
+document.getElementById("btnReclamarTV").addEventListener("click", function () {
+
+  if (derechosReclamados) return;
+
+  const montoDerechosTV = derechosTV?.monto || 1500000;
+
+  if (!presupuestosEquipos[equipoUsuario]) {
+    presupuestosEquipos[equipoUsuario] = 0;
+  }
+
+  presupuestosEquipos[equipoUsuario] += montoDerechosTV;
+  sumarPresupuesto(montoDerechosTV);
+
+  balanceEconomico.derechos += montoDerechosTV;
+  actualizarBalanceUI();
+
+  alert(` Derechos de TV pagados + $${montoDerechosTV.toLocaleString()}`);
+
+  derechosReclamados = true;
+  verificarCerrarModal();
+});
+
+//Pagar 
+document.getElementById("btnPagarSueldos").addEventListener("click", function () {
+
+  if (!derechosReclamados) {
+    alert("Primero debes reclamar los Derechos de TV.");
+    return;
+  }
+
+  if (decisionSueldosTomada) return;
+
+  const sueldoAnual = calcularSueldoPorFuerza(
+    obtenerFuerzaTotal(equipoUsuario)
+  );
+
+  if (presupuestoVisible >= sueldoAnual) {
+
+    restarPresupuesto(sueldoAnual);
+
+    balanceEconomico.sueldos -= sueldoAnual;
+    actualizarBalanceUI();
+
+    alert(` Sueldos pagados: $${sueldoAnual.toLocaleString()}`);
+
+  } 
+  
+  if (presupuestoVisible < sueldoAnual) {
+
+    restarPresupuesto(sueldoAnual);
+
+    balanceEconomico.sueldos -= sueldoAnual;
+    actualizarBalanceUI();
+
+    alert(` Sueldos pagados: $${sueldoAnual.toLocaleString()}`);
+
+  } 
+  /*
+  else {
+    alert("No tienes suficiente presupuesto. Se aplicará penalización.");
+    penalizarPorNoPagarSueldos();
+  }
+    */
+
+  decisionSueldosTomada = true;
+  verificarCerrarModal();
+});
+
+//No Pagar
+document.getElementById("btnNoPagarSueldos").addEventListener("click", function () {
+
+  if (!derechosReclamados) {
+    alert("Primero debes reclamar los Derechos de TV.");
+    return;
+  }
+
+  if (decisionSueldosTomada) return;
+
+  
+    const plantilla = plantillasJugadores[equipoUsuario];
+    let efecto = "";
+
+    plantilla.forEach(j => j.media = Math.max(1, j.media - 4));
+    efecto = "😞 Los jugadores estan desmotivados. -4 de media para toda la plantilla.";
+
+    alert(efecto);
+  //penalizarPorNoPagarSueldos();
+
+  decisionSueldosTomada = true;
+  verificarCerrarModal();
+});
+
+
+//alquiler
+document.getElementById("btnPagarAlquiler").addEventListener("click", function () {
+
+  if (alquilerPagado) return;
+
+  let montos = 1800000;
+  let montoss = 1000000;
+
+  if (!equipoUsuario) {
+    alert("No has elegido un equipo todavia.");
+    return;
+  }
+
+  if (equipoUsuario === "Cali") {
+
+    alert("Debes pagar el terreno del estadio 1M");
+
+    restarPresupuesto(montoss);
+    balanceEconomico.alquiler -= montoss;
+
+  } else {
+
+    alert("Debes pagar el alquiler del estadio 1.8M");
+
+    restarPresupuesto(montos);
+    balanceEconomico.alquiler -= montos;
+
+  }
+
+  actualizarBalanceUI();
+
+  alquilerPagado = true;
+  this.disabled = true;
+
+  verificarCerrarModal();
+});
+
+
+function verificarCerrarModal() {
+
+  if (derechosReclamados && decisionSueldosTomada && alquilerPagado) {
+    document.getElementById("modalEconomico").style.display = "none";
+  }
+}
